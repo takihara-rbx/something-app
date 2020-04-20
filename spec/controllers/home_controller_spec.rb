9afd2e4efc752f9af9acc6d28f47f2b9ab277145
KodaiTakihara <takihara@rabbix.jp>
2020-04-20 18:08:19 +0900

    [add] home top

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe HomeController, type: :controller do
+  # responce successful
+  describe "#top" do
+    it "responce is to be successful" do
+      get :top
+      expect(response).to be_successful
+    end
+  end
+
+  # status code 200
+  it "status code is to have status code 200" do
+    get :top
+    expect(response).to have_http_status "200"
+  end
+end
